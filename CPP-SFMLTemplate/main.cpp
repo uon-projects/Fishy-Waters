8373dd0b401889c5d6dba83792df6c26e1486eaa
Teodor G <grigor.teodor@gmail.com>
2020-01-22 11:25:15 +0000

    v1.0.0

---
+++ CPP-SFMLTemplate/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <SFML/Graphics.hpp>
+
+//Compiler Directives
+using namespace std;
+using namespace sf;
+
+RenderWindow window(VideoMode(800, 500), "Dangerous Racing"); //the game screen
+
+int main()
+{
+
+	//for closing the debug window when the game window is closed
+	getchar();
+
+	return 0;
+}
