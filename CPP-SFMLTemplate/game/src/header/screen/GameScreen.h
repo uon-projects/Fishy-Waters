b798f4bf0a4322fda082bcbfcf6beffacc41d7f1
Teodor G <grigor.teodor@gmail.com>
2020-08-16 12:44:02 +0300

    created map creator

--- CPP-SFMLTemplate/game/src/header/screen/GameScreen.h
+++ CPP-SFMLTemplate/game/src/header/screen/GameScreen.h
@@ -27,6 +27,7 @@ private:
     int mColorEndPortal;
     bool mColorAscending;
     int mBackgroundR, mBackgroundG, mBackgroundB;
+    int **mWaterMap;
 
 public:
     GameScreen()
@@ -61,13 +62,21 @@ public:
         gameMenuScreenBackground.setSize(Vector2f((float) window.getSize().x, (float) window.getSize().y));
         window.draw(gameMenuScreenBackground);
 
-        vector < ItemModel * > mLvlItems = mGameMap->getItemsByLvl();
-        for (ItemModel *mItem : mLvlItems)
+        for (i = 0; i < 40; i++)
         {
-            item.setFillColor(Color(168, 12, 147, 150));
-            item.setPosition(Vector2f((float) mItem->getStartPos().x, (float) mItem->getStartPos().y + mGameOffsetY));
-            item.setSize(Vector2f((float) mItem->getSize().x, (float) mItem->getSize().y));
-            window.draw(item);
+            for (j = 0; j < 40; j++)
+            {
+                int mTileType = mWaterMap[i][j];
+                item.setFillColor(Color(168, 12, 147));
+                item.setPosition(
+                        Vector2f(
+                                (float) i*40,
+                                (float) j*40
+                        )
+                );
+                item.setSize(Vector2f(40.0f, 40.0f));
+                window.draw(item);
+            }
         }
 
         vector < ItemModel * > mEndPortals = mGameMap->getEndPortalByLvl();
@@ -100,7 +109,7 @@ public:
             window.draw(mNPCharacter->getSprite());
         }
 
-        window.draw(mMainCharacter->getSprite());
+        window.draw(mMainCharacter->getSprite(window));
 
         for (MainCharacterBullet *mMainCharacterBullet : mMainCharacterBullets)
         {
@@ -133,62 +142,11 @@ public:
         mMainCharacter = mApp->getMainCharacter();
         mGameMap = mApp->getGameMap();
         mMainCharacter->setGameMap(mApp->getGameMap());
-    }
-
-    void shootBullets(int type)
-    {
-
-        MainCharacterBullet *mMainCharacterBullet = new MainCharacterBullet();
-        Vector2f mPosition;
-        mPosition.x = mMainCharacter->getCharacterPosition().x;
-        mPosition.y = mMainCharacter->getGameHeight();
-        if (type == 1)
-        {
-            mPosition.y -= 53;
-            if (mMainCharacter->faceRight())
-            {
-                mPosition.x = mPosition.x + 15;
-            } else
-            {
-                mPosition.x = mPosition.x - 15;
-            }
-            mMainCharacterBullet->init(
-                    mPosition,
-                    type,
-                    mMainCharacter->faceRight(),
-                    1,
-                    4.0f * 1.4f,
-                    0.3f
-            );
-        } else if (type == 2)
-        {
-            mPosition.y -= 55;
-            if (mMainCharacter->faceRight())
-            {
-                mPosition.x = mPosition.x + 15;
-            } else
-            {
-                mPosition.x = mPosition.x - 15;
-            }
-            mMainCharacterBullet->init(
-                    mPosition,
-                    type,
-                    mMainCharacter->faceRight(),
-                    2,
-                    4.0f * 0.8f,
-                    0.4f
-            );
-        }
-        mMainCharacterBullets.push_back(mMainCharacterBullet);
-
+        mWaterMap = mGameMap->getGameMap();
     }
 
     void inputListener(Event event)
     {
-        if (event.key.code == Keyboard::Space)
-        {
-            mMainCharacter->jump(650.0f);
-        }
         if (event.key.code == Keyboard::X)
         {
             system("pause");
@@ -199,13 +157,6 @@ public:
         } else if (event.key.code == Keyboard::Right || event.key.code == Keyboard::D)
         {
             mMainCharacter->move(20.0f);
-        }
-        if (event.key.code == Keyboard::Num1 || event.key.code == Keyboard::Numpad1)
-        {
-            shootBullets(1);
-        } else if (event.key.code == Keyboard::Num2 || event.key.code == Keyboard::Numpad2)
-        {
-            shootBullets(2);
         }
     }
 
