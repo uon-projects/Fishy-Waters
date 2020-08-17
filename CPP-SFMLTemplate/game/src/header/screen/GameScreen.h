e9339e6d600561cb14f1fd3019b8b242d25c6de9
Teodor G <grigor.teodor@gmail.com>
2020-08-17 15:27:52 +0300

    updated map island color

--- CPP-SFMLTemplate/game/src/header/screen/GameScreen.h
+++ CPP-SFMLTemplate/game/src/header/screen/GameScreen.h
@@ -59,6 +59,8 @@ public:
         Vector2i mLoc;
         Vector2i mGridAreaSurface;
         Vector2f mCharacterPosGrid;
+        Vector2i mStartGridArea;
+        Vector2i mCharacterPosOnGrid;
         RectangleShape item;
 
         gameMenuScreenBackground.setFillColor(Color(mBackgroundR, mBackgroundG, mBackgroundB));
@@ -84,7 +86,18 @@ public:
             mCharacterPosGrid.y = 40 - mCharacterPosGrid.y * -1;
         }
         mCharacterPosGrid.y = (int) mCharacterPosGrid.y % 40;
-        Vector2i mStartGridArea;
+
+        mCharacterPosOnGrid.x = mCharacterPosGrid.x;
+        mCharacterPosOnGrid.y = mCharacterPosGrid.y;
+        if (mCharacterPosOnGrid.x < 0)
+        {
+            mCharacterPosOnGrid.x = 40 + mCharacterPosGrid.x;
+        }
+        if (mCharacterPosOnGrid.y < 0)
+        {
+            mCharacterPosOnGrid.y = 40 + mCharacterPosGrid.y;
+        }
+
         mStartGridArea.x = mCharacterPosGrid.x - mGridAreaSurface.x / 2;
         mStartGridArea.y = mCharacterPosGrid.y - mGridAreaSurface.y / 2;
         if (mStartGridArea.x < 0)
@@ -133,13 +146,40 @@ public:
             mLoc.y = -1;
             for (j = mStartGridArea.y; j <= mEndArea.y; j++)
             {
+
                 int mTileType = mWaterMap[j % 40][i % 40];
+                int mTileN, mTileE, mTileS, mTileW;
+
                 if (mTileType == 0)
                 {
                     item.setFillColor(Color(3, 90, 252));
                 } else if (mTileType == 1)
                 {
-                    item.setFillColor(Color(252, 240, 3));
+                    Vector2i mItemPos(i, j);
+                    mTileS = mWaterMap[(mItemPos.y + 1) % 40][(mItemPos.x) % 40];
+                    mTileE = mWaterMap[(mItemPos.y) % 40][(mItemPos.x + 1) % 40];
+                    if (mItemPos.y - 1 < 0)
+                    {
+                        mTileN = mWaterMap[(40 - mItemPos.y - 1) % 40][(mItemPos.x) % 40];
+                    } else
+                    {
+                        mTileN = mWaterMap[(mItemPos.y - 1) % 40][(mItemPos.x) % 40];
+                    }
+                    if (mItemPos.x - 1 < 0)
+                    {
+                        mTileW = mWaterMap[(mItemPos.y) % 40][(40 - mItemPos.x - 1) % 40];
+                    } else
+                    {
+                        mTileW = mWaterMap[(mItemPos.y) % 40][(mItemPos.x - 1) % 40];
+                    }
+
+                    if (mTileN == 0 || mTileE == 0 || mTileS == 0 || mTileW == 0)
+                    {
+                        item.setFillColor(Color(252, 240, 3, 230));
+                    } else
+                    {
+                        item.setFillColor(Color(252, 240, 3));
+                    }
                 }
                 item.setPosition(
                         Vector2f(
@@ -156,22 +196,21 @@ public:
 
         window.draw(mMainCharacter->getSprite(window));
 
-        cout<<mWindowSize.x/40<<' '<<mWindowSize.y/40<<'\n';
-        for(i=-1; i<mWindowSize.x/40 + 2; i++)
+        for (i = -1; i < mWindowSize.x / 40 + 2; i++)
         {
-            for(j=-1; j<mWindowSize.y/40 + 3; j++)
+            for (j = -1; j < mWindowSize.y / 40 + 3; j++)
             {
-				item.setOutlineColor(Color::Black);
-				item.setOutlineThickness(1);
+                item.setOutlineColor(Color::Black);
+                item.setOutlineThickness(1);
                 item.setFillColor(Color(3, 252, 32, 0));
                 item.setPosition(
                         Vector2f(
-                                i*40 - 20,
-                                j*40 - 10
+                                i * 40 - 20,
+                                j * 40 - 10
                         )
                 );
                 item.setSize(Vector2f(40.0f, 40.0f));
-                window.draw(item);
+//                window.draw(item);
             }
         }
 
